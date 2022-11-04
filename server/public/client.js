35310c7bc1fa52918df17fca180599fab811373f
Paolo Debuque <pdebuque@gmail.com>
2022-11-04 08:18:47 -0500

    styling of history section; working on clear history functionality

--- server/public/client.js
+++ server/public/client.js
@@ -2,44 +2,111 @@ $(document).ready(onReady)
 
 
 function onReady() {
-    console.log('jquery ready')
+    console.log('jquery ready');
+    $('.number').on('click', clickNumber);
     $('.operator').on('click', setOperator);
-    $('#equal').on('click', postEqual);
-    $('#clear').on('click', clearInputs);
+    $('#equal').on('click', clickEqual);
+    $('#clear').on('click', clear);
+    $('#clear-history').on('click', clearHistory);
 }
 
-let operator;
+// ------------------------------------------ CLIENT-SIDE CALC FUNCTIONALITY -------------------------------
 
+const calcState = {
+    num1: '',
+    num2: '',
+    operator: '',
+    inputField: ''
+}
+
+const operators = ['/', '*', '+', '-']
+// clicking a number concatenates its data onto the input field, then renders the input
+function clickNumber() {
+    // after operator entry, need to clear the inputField
+
+    if (operators.includes(calcState.inputField)) {
+        calcState.inputField = '';
+        renderInput()
+    }
+
+    if (calcState.inputField.length < 8) {
+        console.log('in clickNumber()');
+        calcState.inputField += $(this).data().val;
+        console.log(calcState.inputField)
+        renderInput();
+    }
+}
+
+function renderInput() {
+    $('#number-field').empty();
+    $('#number-field').html(calcState.inputField);
+}
+
+// upon clicking an operator, save the inputfield as num1, clear the results input, replace with the operation at hand, 
 function setOperator() {
-    console.log('in updateOperator()')
-    operator = $(this).data().operator;
-    console.log('the operator is now', operator)
+    console.log('in setOperator()');
+    calcState.num1 = calcState.inputField;
+    calcState.inputField = $(this).data().symbol;
+    calcState.operator = $(this).data().operator;
+    console.log('current state: ', calcState);
+    renderInput();
 }
 
-function postEqual() {
+function clickEqual() {
+    // save inputField as num2 and clear inputField
+    calcState.num2 = calcState.inputField;
+    calcState.inputField = ''
+
+    renderInput()
+    // with everything packaged, send to postEqual
+    postEqual(calcState);
+}
+
+
+function clear() {
+    // set input field to zero, set all values to zero, render
+    calcState.num1 = '';
+    calcState.num2 = '';
+    calcState.operator = '';
+    calcState.inputField = '';
+    renderInput();
+}
+
+
+// --------------------------------------------- SERVER INTERACTIONS ---------------------------------------------
+
+// upon clicking equal, save the inputfield as num2, clear the results input, make the POST request
+function postEqual(state) {
     // get values
+    // error validation: only post if num1 and num2 exist
+    if (!calcState.num1 || !calcState.num2) {
+        calcState.inputField = 'ERROR';
+        renderInput();
+    } else {
 
-    $.ajax({
-        type: 'POST',
-        url: '/calculate',
-        data: {
-            num1: $('#num1').val(),
-            num2: $('#num2').val(),
-            operation: operator
-        }
-    }).then((res) => {
-        console.log('received data: ', res);
-        getResult();
-    }).catch((err) => {
-        console.log('posting went wrong!', err)
-    })
-    // send ajax POST request
+        $.ajax({
+            type: 'POST',
+            url: '/calculate',
+            data: {
+                num1: state.num1,
+                num2: state.num2,
+                operation: state.operator
+            }
+        }).then((res) => {
+            console.log('received data: ', res);
+            getResult();
+        }).catch((err) => {
+            console.log('posting went wrong!', err)
+        })
+        // send ajax POST request
 
-    // in then, send ajax POST to get the result
+        // in then, send ajax POST to get the result
 
-    // clear inputs
-    $('#num1').val('');
-    $('#num2').val('');
+        // clear inputs
+        $('#num1').val('');
+        $('#num2').val('');
+    }
+    // set all state values to default
 }
 
 // this function sends a GET request to the server, which has calculated the result
@@ -57,9 +124,16 @@ function getResult() {
 
 }
 
-function clearInputs() {
-    $('#num1').val('');
-    $('#num2').val('');
+function clearHistory() {
+    console.log('in clearHistory()');
+    $.ajax({
+        type: 'DELETE',
+        url: 'calculate'
+    }).then((res) => {
+        console.log('successfully cleared history');
+    }).catch((err) => {
+        console.log('could not delete: ', err)
+    })
 }
 
 // takes the array and renders to the display. 
@@ -71,10 +145,29 @@ function clearInputs() {
 // }
 
 function renderDisplay(array) {
-    $('#results').empty();
+    $('#number-field').html(array[array.length - 1].result);
+    $('#history').empty();
 
     for (let calculation of array) {
-        $('#results').append(`
-        ${calculation.num1} ${calculation.operation} ${calculation.num2} = ${calculation.result}`);
+        $('#history').append(`
+        <div>
+            ${calculation.num1} ${calculation.operation} ${calculation.num2} = ${calculation.result}
+        </div>    
+            `);
+
     }
 }
+
+
+// STRETCH
+
+// CONVERT INTERFACE
+// limit characters in the number field - both for inputs and outputs ✅ outputs solved with scrolling
+// add decimals
+// more bug fixes - what happens when you hit two operators in a row?
+
+// POST only if inputs are ready ✅
+
+// create clear history button (DELETE request) 
+
+// arm entries on history list
